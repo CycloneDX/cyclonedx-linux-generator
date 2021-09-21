1d665f14e789262d9a036cfd304b0c538b473764
William R. Goff <william.r.goff@lmco.com>
2021-09-21 10:46:20 -0400

    feature: Added capability to pass in image url.
    Signed-off-by: William R. Goff <william.r.goff@lmco.com>

--- src/test/java/org/cyclonedx/contrib/com/lmco/efoss/unix/sbom/UnixSbomGeneratorApplicationTests.java
+++ src/test/java/org/cyclonedx/contrib/com/lmco/efoss/unix/sbom/UnixSbomGeneratorApplicationTests.java
@@ -1,13 +1,123 @@
+/*
+ * Copyright (c) 2018,2019 Lockheed Martin Corporation.
+ *
+ * This work is owned by Lockheed Martin Corporation. Lockheed Martin personnel are permitted to use and
+ * modify this software.  Lockheed Martin personnel may also deliver this source code to any US Government
+ * customer Agency under a "US Government Purpose Rights" license.
+ *
+ * See the LICENSE file distributed with this work for licensing and distribution terms
+ */
 package org.cyclonedx.contrib.com.lmco.efoss.unix.sbom;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import java.io.ByteArrayOutputStream;
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.Date;
 
-@SpringBootTest
+import org.cyclonedx.contrib.com.lmco.efoss.sbom.commons.test.utils.Log4JTestWatcher;
+import org.cyclonedx.contrib.com.lmco.efoss.sbom.commons.test.utils.TestUtils;
+import org.cyclonedx.contrib.com.lmco.efoss.sbom.commons.utils.DateUtils;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@ActiveProfiles("sandbox")
 class UnixSbomGeneratorApplicationTests {
 
-	@Test
-	void contextLoads() {
+	private static final String LOG4J_FILE = "UnixSbomGeneratorApplicationAppender.xml";
+
+	@Rule
+	public static Log4JTestWatcher watcher = new Log4JTestWatcher(LOG4J_FILE,
+					"UnixSbomGeneratorApplicationTests");
+
+	private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+	/**
+	 * Turns on stdOut output capture
+	 */
+	private void captureOut()
+	{
+		System.setOut(new PrintStream(outContent));
 	}
 
+	/**
+	 * Turns on stdErr output capture
+	 */
+	private void captureErr()
+	{
+		System.setErr(new PrintStream(errContent));
+	}
+
+	/**
+	 * Turns off stdOut capture and returns the contents that have been captured
+	 *
+	 * @return String the contents of our stdOut.
+	 */
+	private String getOut()
+	{
+		System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+		return outContent.toString().replaceAll("\r", "");
+	}
+
+	/**
+	 * Turns off stdErr capture and returns the contents that have been captured
+	 *
+	 * @return
+	 */
+	private String getErr()
+	{
+		System.setErr(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+		return errContent.toString().replaceAll("\r", "");
+	}
+
+	/**
+	 * (U) This method is used to test to make sure our help menu works. It does NOT
+	 * check the contents, but check to make sure it begins "usage: help".
+	 */
+	@Test
+	public void testHelp()
+	{
+		String methodName = new Object()
+		{
+		}.getClass().getEnclosingMethod().getName();
+
+		Date startDate = DateUtils.rightNowDate();
+
+		TestUtils.logTestStart(methodName, watcher.getLogger());
+
+		String helpUsageText = "usage: help";
+
+		try
+		{
+			captureOut();
+
+			String[] args = new String[] {
+				"-h"
+			};
+			UnixSbomGeneratorApplication.main(args);
+
+			String theOutput = getOut();
+
+			watcher.getLogger().debug("Help Test: \n" + theOutput);
+
+			Assert.assertTrue(theOutput.contains(helpUsageText));
+		}
+		catch (Exception e)
+		{
+			String error = "Unexpected error occured while attempting to check to make sure our " +
+							"help menu works!";
+			watcher.getLogger().error(error, e);
+			Assert.fail(error);
+		}
+		finally
+		{
+			TestUtils.logTestFinish(methodName, startDate, watcher.getLogger());
+		}
+	}
 }
